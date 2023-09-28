da2fc79203ec5cbecc80db7058746c095f0ef383
Mochi McCoy <hi@mochibun.me>
2023-09-28 19:42:37 +0200

    Navbar work, WIP

--- src/components/HackNav.jsx
+++ src/components/HackNav.jsx
@@ -1,25 +1,50 @@
-import React from "react";
-
 import { Avatar, Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Dropdown, DropdownItem, DropdownTrigger, DropdownMenu, Input, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@nextui-org/react";
 import {AcmeLogo} from "../AcmeLogo.jsx";
 import { SearchIcon } from "../SearchIcon.jsx";
+import { useState, useEffect } from "react";
 
 export default function HackNav() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [signedIn, setSignedIn] = useState(false);
+
+  const noUserIcon = "https://media.discordapp.net/attachments/199274450011553792/1156984505408700417/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.png"; 
+  const emailPlaceholder = "hi@placehold.er";
+  const userIconPlaceholder = "https://i.pravatar.cc/150";
+
+
+
+  const signInClick = () => {
+    setSignedIn(!signedIn);
+  };
 
   const menuItems = [
-    "New",
+    "New", //index 0
     "Past",
     "Comments",
     "Ask",
     "Show",
     "Submit",
-    "",
+    "", //index 6
     "Settings",
     "Jobs",
     "Help & Feedback",
-    "Log Out",
+    "Sign Out", //index 10
+    "Sign In" //index 11
   ];
+
+  const LinkTemplate = ({i}) => {
+    return(
+        <NavbarMenuItem>
+          <Link
+          className="w-full"
+          color="foreground"
+          href="#"
+          size="lg">
+            {menuItems[i]}
+          </Link>
+        </NavbarMenuItem>
+      )
+    };
 
   return (
     <Navbar
@@ -97,29 +122,63 @@ export default function HackNav() {
               color="primary"
               name="Jason Hughes"
               size="sm"
-              src="https://i.pravatar.cc/150" //Placeholder data
+              src={ signedIn ? userIconPlaceholder : noUserIcon } //Placeholder data
             />
           </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">placeholder@da.ta</p> {/* Placeholder data */}
+              <p className="font-semibold">{signedIn ? `Signed in as:` : `Not signed in`}</p>
+              {signedIn && <p className="font-semibold">{emailPlaceholder}</p>}
             </DropdownItem>
             <DropdownItem key="settings">{menuItems[7]}</DropdownItem>
             <DropdownItem key="team_settings">{menuItems[8]}</DropdownItem>
             <DropdownItem key="analytics">{menuItems[9]}</DropdownItem>
-            <DropdownItem key="logout" color="danger">{menuItems[10]}</DropdownItem>
+            <DropdownItem key="logout" color={signedIn ? "danger" : "warning"} onClick={signInClick}>{signedIn ? menuItems[10] : menuItems[11]}</DropdownItem>
           </DropdownMenu>
         </Dropdown>
       </NavbarContent>
 
       <NavbarMenu>
-        {menuItems.map((item, index) => (
+        
+        <NavbarMenuItem>
+          {signedIn && <p className="text-xs">Signed in as: <b>{emailPlaceholder}</b></p>} {/* Placeholder data */}
+        </NavbarMenuItem>
+        
+        <NavbarMenuItem>
+          <Link
+          className="w-full"
+          color="primary"
+          href="#"
+          size="lg">
+            {menuItems[0]}
+          </Link>
+        </NavbarMenuItem>
+          <LinkTemplate i={1}/>
+          <LinkTemplate i={2}/>
+          <LinkTemplate i={3}/>
+          <LinkTemplate i={4}/>
+          <LinkTemplate i={5}/>
+          <br/>
+          <LinkTemplate i={7}/>
+          <LinkTemplate i={8}/>
+          <LinkTemplate i={9}/>
+          <NavbarMenuItem>
+        <Link
+          className="w-full"
+          color={ signedIn ? "danger" : "warning"}
+          href="#"
+          size="lg"
+          onClick={signInClick}>
+            { signedIn ? menuItems[10] : menuItems[11] }
+          </Link>
+        </NavbarMenuItem>
+
+        {/* {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
               className="w-full"
               color={
-                index === 0 ? "success" : index === menuItems.length - 1 ? "danger" : "foreground"
+                index === 0 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
               }
               href="#"
               size="lg"
@@ -127,7 +186,7 @@ export default function HackNav() {
               {item}
             </Link>
           </NavbarMenuItem>
-        ))}
+        ))} */}
       </NavbarMenu>
     </Navbar>
   );
